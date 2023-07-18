b877a5c1719889fd9acd29175d005b6bb4b6e7a5
Tomislav Sertic <tsertic5@gmail.com>
2023-07-18 10:55:31 +0200

    server setup

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const dotenv = require("dotenv").config();
 
 PORT = process.env.PORT || 4448;
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
 app.listen(PORT, () => {
   console.log(
     `Server up in ${process.env.NODE_ENV} mode and running on: http://127.0.0.1:${PORT}`
