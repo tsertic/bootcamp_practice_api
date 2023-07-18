68a041a8eec8de5be5e2a3a4a77e54daaacdcbbe
Tomislav Sertic <tsertic5@gmail.com>
2023-07-18 07:24:28 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+
+PORT = process.env.PORT || 4448;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server up in ${process.env.NODE_ENV} mode and running on: http://127.0.0.1:${PORT}`
+  );
+});
