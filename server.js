c7aaa9fb1c0457a57adf7283a58151c031597c63
tsertic <tsertic5@gmail.com>
2023-07-19 08:25:14 +0200

    added controllers and routes for bootcamps

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const router = require("./routes/bootcamps");
 const app = express();
 const dotenv = require("dotenv").config();
-
+const morgan = require("morgan");
+const { logger } = require("./middleware/logger");
 PORT = process.env.PORT || 4448;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-});
+//DEV logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
+app.use("/api/v1/bootcamps", router);
+app.get("/", (req, res) => {
+  console.log(req.protocol, req.originalUrl);
+});
 app.listen(PORT, () => {
   console.log(
     `Server up in ${process.env.NODE_ENV} mode and running on: http://127.0.0.1:${PORT}`
